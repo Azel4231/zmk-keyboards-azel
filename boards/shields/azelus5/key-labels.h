33025ab375b1838fb396cda46b11df24ca298cb6
Azel4231 <Azel4231@users.noreply.github.com>
2025-12-20 13:23:06 +0100

    HRM Test Layer

---
+++ boards/shields/azelus5/key-labels.h
@@ -0,0 +1,57 @@
+#define LN2 0
+
+#define LT6 2
+#define LT5 3
+#define LT4 4
+#define LT3 5
+#define LT2 6
+#define LT1 7
+
+#define LM6 14
+#define LM5 15
+#define LM4 16
+#define LM3 17
+#define LM2 18
+#define LM1 19
+
+#define LB6 26
+#define LB5 27
+#define LB4 28
+#define LB3 29
+#define LB2 30
+#define LB1 31
+
+#define LH4 38
+#define LH3 39
+#define LH2 40
+#define LH1 41
+#define LH0 42
+
+#define RN2 1
+
+#define RT6 8
+#define RT5 9
+#define RT4 10
+#define RT3 11
+#define RT2 12
+#define RT1 13
+
+#define RM6 20
+#define RM5 21
+#define RM4 22
+#define RM3 23
+#define RM2 24
+#define RM1 25
+
+#define RB6 32
+#define RB5 33
+#define RB4 34
+#define RB3 35
+#define RB2 36
+#define RB1 37
+
+#define RH4 43
+#define RH3 44
+#define RH2 45
+#define RH1 46
+#define RH0 47
